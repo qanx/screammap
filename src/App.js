81e90d6e410504023efab199f7bd64bd2f18a1da
qanx <amfqqq@gmail.com>
2021-07-16 23:29:10 +0300

    input done , key value still

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import './App.css'
 import {useState,useEffect} from 'react'
 import Cir from './components/cir/cir.jsx'
-
+import Input from './components/input/input'
 
 
 
  const App=()=> {
   
-  const [items, setItems] = useState([]);
-
+  const [items, setItems] = useState([  {id: 0, x: 16.893514811849478, y: 18.583042973286876 ,text:"d"},
+    {id: 1, x: 52.602081665332264, y: 38.79210220673635 ,text:"d"}]);
+  const [input, setInput] = useState('')
 
   const getPos = (e) => {
+
+    if (input===""){
+      console.warn("enter value")
+    }
+    else{
     let x = (e.pageX * 100) / window.innerWidth;
     let y = (e.pageY * 100) / window.innerHeight;
+    let text=input;
     console.log(x + "% - " + y);
     // setItems({x,y})
-    setItems([...items,{id:items.length,x,y}],checkiing()
+    setItems([...items,{id:items.length,x,y,text:text}],checkiing()
+   
    )
+   setInput("")
 
   //   (prevState =>{
   //     return{
@@ -27,7 +36,7 @@ import Cir from './components/cir/cir.jsx'
       
  
   }
-
+}
 
 
   //useing effect
@@ -42,8 +51,8 @@ const checkiing=()=>{
 
   return (
     <div className="App" >
+      <Input input={input} setInput={setInput}  items={items}/>
       
-      <input />
       {items.map(items=> <Cir key={items.id} items={items}/>)}
       
       <img  onClick={getPos } src="https://i.picsum.photos/id/213/536/354.jpg?hmac=uR4LvY7i-5xHRT95H_5axefzY962e0SahoabNthg3zg"/>
